9a72d0de7563404c2d6e2831ad7ac23f26b1f1df
Meghthedev <meghthedev@gmail.com>
2023-04-05 22:40:33 +0530

    videomaker: WIP: add function to caption video

--- main.py
+++ main.py
@@ -105,3 +105,58 @@ def cut_vid():
             break
     cap.release()
     out.release()
+
+def caption():
+    # Load audio and get its duration
+    audio_path = 'output.mp3'
+    video_path = 'cutvideo.mp4'
+    text_path = 'quote.txt'
+
+    # Load the audio file
+    audio_clip = AudioFileClip(audio_path)
+
+    # Load the text from the file
+    with open("quote.txt") as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    # Define the output video file
+    out = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc(*'mp4v'), audio_clip.fps, (1920,1080))
+
+    # Set the font and the font size for the captions
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_size = 1
+    font_thickness = 2
+
+    # Set the initial timestamp
+    current_time = 0
+
+    # Process each line of text and add the caption to the video
+    for line in lines:
+        # Get the duration of the current line
+        duration = len(line) / 10
+
+        # Get the end time of the current line
+        end_time = current_time + duration
+
+        # Generate the caption image
+        caption_img = generate_caption_image(line, font, font_size, font_thickness)
+
+        # Add the caption to the video for the duration of the current line
+        while current_time < end_time:
+            # Get the current frame
+            frame = get_frame(current_time)
+
+            # Add the caption to the frame
+            frame = add_caption_to_frame(frame, caption_img)
+
+            # Write the frame to the output video
+            out.write(frame)
+
+            # Increment the current time
+            current_time += 1 / audio_clip.fps
+
+    # Release the video writer
+    out.release()
+
+    # Close the audio file
+    audio_clip.close()
